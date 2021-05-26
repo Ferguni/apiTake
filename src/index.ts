c97712819421b8ea46f3b6cc80e4acf0775df64a
Edson Carlos <edsoncarlos.ec40@gmail.com>
2021-05-26 14:30:29 -0300

    Primeiro commit

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import express from 'express'
+import http from 'http'
+import cors from 'cors'
+import api from './api'
+
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+
+app.get('/', async (req, res) => {
+
+  
+  res.redirect(`https://api.github.com/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
+
+})
+
+
+
+app.get('/:img', async (req, res) => {
+
+  
+  res.redirect(`https://avatars.githubusercontent.com/u/4369522?v=4`)
+  //Just redirect  the avatar_url because is the same for all repositories
+
+})
+
+app.get('/name/:name', async (req, res) => {
+
+  
+  const response = await api.get(`/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
+
+  const { name } = response.data[req.params.name]
+
+
+
+
+  const formatJson = {
+    name
+  }
+
+  
+  
+  res.json(formatJson)
+})
+
+
+
+app.get('/description/:description', async (req, res) => {
+  const param = req.params
+
+  const response = await api.get(`/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
+
+  const { description } = response.data[req.params.description]
+
+
+
+
+  const formatJson = {
+    description
+  }
+  
+  
+  res.json(formatJson)
+})
+
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
