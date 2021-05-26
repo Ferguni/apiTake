a398a93692c4aa1e2411c5a7e7ec1fb2aabf4ca7
Edson Carlos <edsoncarlos.ec40@gmail.com>
2021-05-26 16:42:48 -0300

    code changes

--- src/index.ts
+++ src/index.ts
@@ -68,8 +68,6 @@ app.get('/description/:description', async (req, res) => {
 })
 
 
-const httpServer = http.createServer(app);
+const port = process.env.PORT || 3000;
 
-httpServer.listen(3000, () => {
-  console.log('Listening on port 3000')
-})
+app.listen(port, () => console.log(`Umbler server at port:${port}`));
