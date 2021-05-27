9128413a911dadbe3b53411a1dacb8444d87376e
Edson Carlos <edsoncarlos.ec40@gmail.com>
2021-05-26 23:57:22 -0300

    make it better

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-import api from './api'
+import express from 'express';
+import cors from 'cors';
+import api from './api';
 
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 
 app.get('/', async (req, res) => {
@@ -16,21 +15,28 @@ app.get('/', async (req, res) => {
   
   res.redirect(`https://api.github.com/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
 
-})
+});
 
 
 
 app.get('/img/:img', async (req, res) => {
 
-  
-  res.redirect(`https://avatars.githubusercontent.com/u/4369522?v=4`)
-  //Just redirect  the avatar_url because is the same for all repositories
+  try {
+    
+    const { img } = req.params
 
-})
+  res.redirect(`https://avatars.githubusercontent.com/u/4369522?v=4`);
+  //Just redirect  the avatar_url because is the same for all repositories
+  } catch (error) {
+    console.error(error.message)
+  }
+
+});
 
 app.get('/name/:name', async (req, res) => {
 
   
+ try {
   const response = await api.get(`/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
 
   const { name } = response.data[req.params.name]
@@ -44,15 +50,20 @@ app.get('/name/:name', async (req, res) => {
 
   
 
-  res.json(formatJson).json
-})
+  res.json(formatJson);
+ } catch (error) {
+  console.error(error.message)
+
+ }
+});
 
 
 
 app.get('/description/:description', async (req, res) => {
-  const param = req.params
+  
 
-  const response = await api.get(`/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
+  try {
+    const response = await api.get(`/orgs/takenet/repos?per_page=5&page=1+language:C#&sort=stars&order=desc`)
 
   const { description } = response.data[req.params.description]
 
@@ -64,7 +75,11 @@ app.get('/description/:description', async (req, res) => {
   }
   
   
-  res.json(formatJson).json
+  res.json(formatJson);
+  } catch (error) {
+    console.error(error.message)
+
+  }
 })
 
 
